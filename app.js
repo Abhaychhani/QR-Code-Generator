d56f0faae11b02809a3000aad6c59428058e48ae
Abhay verma <abhaychhani@gmail.com>
2024-12-19 03:31:33 +0530

    fixes console errors and added validation

--- app.js
+++ app.js
@@ -7,31 +7,35 @@ downloadBtn.style.opacity = 0.3;
 downloadBtn.style.pointerEvents = "none";
 
 function GenerateQR() {
+  if(urlInput.value==""){
+    const ele = document.createElement('p')
+    ele.innerText="Please Enter somthing! ";
+    ele.classList.add('validation')
+    urlInput.insertAdjacentElement("afterend",ele);
+    return;
+  }
   imageHolder.alt = urlInput.value;
   let url = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${urlInput.value}`;
-  console.log("url", url);
+  
   fetch(url)
     .then((response) => response.blob())
     .then((blob) => {
-      console.log(blob);
       const url = URL.createObjectURL(blob);
       imageHolder.src = url;
       imageHolder.style.opacity = "1";
       downloadBtn.disabled = false;
       downloadBtn.style.pointerEvents = "all";
       downloadBtn.style.opacity = "1";
-
       downloadBtn.addEventListener("click", () => {
         const link = document.createElement("a");
         link.href = url;
         link.download = `qrImage.png`;
-        console.log(link);
         link.click();
         URL.revokeObjectURL(url);
       });
     })
     .catch((err) => {
-      console.log(err);
+      document.body.innerHTML="<h1 style='color:white; padding:2rem;'>Somthing Went Wrong:(</h1>";
     });
 }
 
